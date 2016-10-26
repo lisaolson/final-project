3d3888c3718b57d785e5386b7f74047dfb8ef63f
Lisa Olson <olson.lisa94@gmail.com>
2016-10-26 13:46:09 -0700

    adds ui-router to redirect route to home page

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var app = angular.module('audelia', []);
+var app = angular.module('audelia', ['ui.router'])
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/home.html',
+          controller: 'MainCtrl'
+        });
+
+        $urlRouterProvider.otherwise('home');
+    }])
+  .factory('posts', [function(){
+    var o = {
+      posts: []
+    };
+    return o;
+  }])
+
   app.controller('MainCtrl', [
     '$scope',
-    function($scope){
+    'posts',
+    function($scope, posts){
 
-      $scope.posts = [
-        {title: 'post 1', username: 'thatsme', postInput: 'whatever i decided to write'},
-        {title: 'post 1', username: 'thatsme', postInput: 'whatever i decided to write'},
-      ];
+      $scope.posts = posts.posts;
 
       $scope.addPost = function() {
+        if ($scope.title === '') { return; }
         $scope.posts.push({
           title: $scope.title,
           username: $scope.username,
           postInput: $scope.postInput
         });
+        $scope.title = '';
+        $scope.username = '';
+        $scope.postInput = '';
       }
     }]);
