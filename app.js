73d10bd71c24e676036df7bb1ba8b474e895b361
Lisa Olson <olson.lisa94@gmail.com>
2016-10-26 16:06:50 -0700

    adds client-side ability to create posts/comment on posts

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var app = angular.module('audelia', ['ui.router'])
           url: '/home',
           templateUrl: '/home.html',
           controller: 'MainCtrl'
+        })
+
+        .state('posts', {
+          url: '/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl'
         });
 
         $urlRouterProvider.otherwise('home');
@@ -32,10 +38,32 @@ var app = angular.module('audelia', ['ui.router'])
         $scope.posts.push({
           title: $scope.title,
           username: $scope.username,
-          postInput: $scope.postInput
+          postInput: $scope.postInput,
+          comments: [
+            {author: 'Joe', body: 'Cool post'},
+            {author: 'Bob', body: 'All wrong'}
+          ]
         });
+
         $scope.title = '';
         $scope.username = '';
         $scope.postInput = '';
+      }
+    }])
+
+  app.controller('PostsCtrl', [
+      '$scope',
+      '$stateParams',
+      'posts',
+    function($scope, $stateParams, posts) {
+      $scope.post = posts.posts[$stateParams.id];
+      //
+      $scope.addComment = function() {
+        if ($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user'
+        });
+        $scope.body = '';
       }
     }]);
