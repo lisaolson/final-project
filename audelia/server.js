45149a10bf58801ff61dc966803df187233986d8
Lisa Olson <olson.lisa94@gmail.com>
2016-10-28 14:09:54 -0700

    adds post  route to sign up user

--- audelia/server.js
+++ audelia/server.js
@@ -5,17 +5,21 @@ var express = require('express');
 //generate new express app called 'app'
 var app = express();
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
 
 //serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-
+app.use(passport.initialize());
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
@@ -26,6 +30,25 @@ mongoose.connect('mongodb://localhost/audelia');
 
 app.get('/', function homepage (req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/register', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
 });
 
 app.get('/posts', function(req, res, next) {
