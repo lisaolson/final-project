24b2b26eb05032b67d895f98fa0c4e4a556b23d8
Lisa Olson <olson.lisa94@gmail.com>
2016-10-28 14:56:41 -0700

    adds post route for /login w/ fctn to check username/password and log them in if successful

--- audelia/server.js
+++ audelia/server.js
@@ -32,7 +32,7 @@ app.get('/', function homepage (req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
+//generating jwt when user is successfully registered
 app.post('/register', function(req, res, next) {
   if(!req.body.username || !req.body.password) {
     return res.status(400).json({message: 'Please fill out all fields'});
@@ -49,6 +49,22 @@ app.post('/register', function(req, res, next) {
 
     return res.json({token: user.generateJWT()})
   });
+});
+
+app.post('/login', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if(err){ return next(err); }
+
+    if (user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 app.get('/posts', function(req, res, next) {
