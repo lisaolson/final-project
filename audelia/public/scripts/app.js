7efe6670a5eb14d459b028ef483cab1099f39fb2
Lisa Olson <olson.lisa94@gmail.com>
2016-10-28 18:13:28 -0700

    adds Auth Controller for login/register

--- audelia/public/scripts/app.js
+++ audelia/public/scripts/app.js
@@ -30,6 +30,84 @@ var app = angular.module('audelia', ['ui.router'])
         $urlRouterProvider.otherwise('home');
     }])
 
+  .factory('auth', ['$http', '$window', function($http, $window) {
+    var auth = {};
+
+    auth.saveToken = function(token) {
+      //unique key to always read/write from
+      //changing it would log everyone out
+      $window.localStorage['audelia-token'] = token;
+    };
+
+    auth.getToken = function() {
+      return $window.localStorage['audelia-token'];
+    }
+    //checks whether user is logged in
+    auth.isLoggedIn = function() {
+      var token = auth.getToken();
+
+      if(token) {
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.exp > Data.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+
+    auth.currentUser = function() {
+      if (auth.isLoggedIn()) {
+        var token = auth.getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+        return payload.username;
+      }
+    };
+
+    //saving token logs user in
+    auth.register = function(user) {
+      return $http.post('/register', user).success(function(data) {
+        auth.saveToken(data.token);
+      });
+    };
+
+    auth.login = function(user) {
+      return $http.post('/login', user).success(function(data) {
+        auth.saveToken(data.token);
+      });
+    };
+
+    auth.logOut = function() {
+      $window.localStorage.removeItem('audelia-token');
+    };
+
+    return auth;
+  }])
+
+  .contoller('AuthCtrl', [
+    '$scope',
+    '$state',
+    '$auth',
+    function($scope, $state, auth){
+      $scope.user = {};
+
+      $scope.register = function() {
+        auth.register($scope.user).error(function(error){
+          $scope.error = error;
+        }).thn(function(){
+          $state.go('home');
+        });
+      };
+
+      $scope.logIn = function(){
+        auth.logIn($scope.user).error(function(error){
+          $scope.error = error;
+        }).then(function(){
+          $state.go('home');
+        });
+      };
+    }])
+
   .factory('posts', ['$http', function($http){
     var o = {
       posts: []
