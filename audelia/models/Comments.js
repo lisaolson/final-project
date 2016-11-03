af3ff5db61b9fe93eaffbb8a4cff8cd3f70662c9
Lisa Olson <olson.lisa94@gmail.com>
2016-11-02 20:43:25 -0700

    adds upvoting feature to model

--- audelia/models/Comments.js
+++ audelia/models/Comments.js
@@ -5,7 +5,13 @@ var Schema = mongoose.Schema;
 var CommentSchema = new Schema({
   comment: String,
   body: String,
+  reports: { type: Number, default: 0 },
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
 
+
+CommentSchema.methods.report = function(cb) {
+  this.reports == 1;
+  this.save(cb);
+}
 mongoose.model('Comment', CommentSchema);
